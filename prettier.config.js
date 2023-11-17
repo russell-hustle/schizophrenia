78ff5e92e41469802f761aa3a79c42ed0007516b
Will Dickinson <will@zonos.com>
2023-11-17 11:17:29 -0700

    idk a new page

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import("prettier").Config}
+ */
+module.exports = {
+  arrowParens: 'avoid',
+  printWidth: 80,
+  proseWrap: 'preserve',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+};
---
+++ src/routes/head-space/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import PartySocket from "partysocket";
+
+  // declare const PARTYKIT_HOST: string;
+
+  import VoiceCreator from "@/components/VoiceCreator.svelte";
+
+  let spaceName = "unknown";
+  let creationOpen = false;
+
+  const handleClose = () => {
+    creationOpen = false;
+  };
+
+//   const conn = new PartySocket({
+//   host: PARTYKIT_HOST,
+//   room: spaceName,
+// });
+
+</script>
+
+<svelte:head>
+  <title>{spaceName}</title>
+  <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<div>
+  <button on:click={() => (creationOpen = true)}>Create Voice</button>
+
+  <VoiceCreator open={creationOpen} handleClose={handleClose} />
+</div>
